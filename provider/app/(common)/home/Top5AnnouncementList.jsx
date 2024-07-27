9225b4347a1bf13ab053d529a6874250aca9351b
IDKNWHORU <junho9341@gmail.com>
2024-07-27 17:35:07 +0900

    공고 목록, 학습 목록 컴포넌트에서 카드 컴포넌트를 사용하도록 변경

--- provider/app/(common)/home/Top5AnnouncementList.jsx
+++ provider/app/(common)/home/Top5AnnouncementList.jsx
@@ -1,6 +1,6 @@
+import Card from "@/components/card/Card";
+import LabelType from "@/components/card/LabelType";
 import fetchWithRetry from "@/functions/api";
-import Link from "next/link";
-import { Fragment } from "react";
 
 async function getTop5Announcement() {
   const getTop5AnnouncementResponse = await fetchWithRetry(
@@ -11,34 +11,28 @@ async function getTop5Announcement() {
     if (getTop5AnnouncementResponse.status === 404) return [];
     else throw new Error("공고 목록을 조회하는 중 에러가 발생했습니다.");
 
-  return await getTop5AnnouncementResponse.json();
+  const announcements = await getTop5AnnouncementResponse.json();
+
+  return announcements.map(({ postingId, title }) => ({
+    id: postingId,
+    title: {
+      link: `/announcement/${postingId}`,
+      text: title,
+    },
+    label: {
+      type: LabelType.Time,
+      text: "마감 N일전",
+    },
+  }));
 }
 
 export default async function Top5AnnouncementList() {
   const announcements = await getTop5Announcement();
 
   return (
-    <>
-      {announcements.map((announcement, index) => (
-        <Fragment key={announcement.postingId}>
-          <li className="article">
-            {/* <div className="article-status">
-          <p className="article-status-text caption-12 color-purple-01">
-          마감 N일전
-          </p>
-        </div> */}
-            <Link
-              className="article-text h4-20"
-              href={`/announcement/${announcement.postingId}`}
-            >
-              {announcement.title}
-            </Link>
-          </li>
-          {index < announcements.length - 1 ? (
-            <hr className="article-divider" />
-          ) : null}
-        </Fragment>
-      ))}
-    </>
+    <Card
+      header={{ link: "/announcement", title: "공고 목록" }}
+      contents={announcements}
+    />
   );
 }
