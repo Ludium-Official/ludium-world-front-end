d01badc75c7bd0df01d55a8bc984b2f332556af0
IDKNWHORU <junho9341@gmail.com>
2024-07-28 22:30:48 +0900

    공고 페이지 스타일 추가

--- provider/components/table/RowWithButton.jsx
+++ provider/components/table/RowWithButton.jsx
@@ -2,9 +2,30 @@ import Link from "next/link";
 import Label from "../label/Label";
 import style from "./row.module.css";
 
-export default function ({ label, title, caption, button }) {
+export default function ({
+  label,
+  title,
+  caption,
+  button,
+  loading,
+  loadingClassName,
+}) {
+  if (loading) {
+    return (
+      <div className={`${loadingClassName} ${style.row}`}>
+        <div className={style.cell}>
+          {label.type ? <Label type={label.type} text={label.text} /> : null}
+          <Link href={title.link}>
+            <h4>{title.text}</h4>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
   const captionClassNames = `${caption.className} ${style.cell}`;
   const buttonClassNames = `${button.className} ${style.cell}`;
+
   return (
     <div className={style.row}>
       <div className={style.cell}>
@@ -21,11 +42,6 @@ export default function ({ label, title, caption, button }) {
           <h5>{button.text}</h5>
         </Link>
       </div>
-      {/* <div className={style["cell-fixed"]}>
-        {fixed.label.type ? (
-          <Label type={fixed.label.type} text={fixed.label.text} />
-        ) : null}
-      </div> */}
     </div>
   );
 }
