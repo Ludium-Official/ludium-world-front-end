839b69bff537bfce532a405a0da9afa2aec369fa
IDKNWHORU <junho9341@gmail.com>
2024-07-28 19:30:03 +0900

    공고 페이지에 공고 목록 스타일 변경

--- provider/app/(common)/announcement/AnnouncementList.jsx
+++ provider/app/(common)/announcement/AnnouncementList.jsx
@@ -1,5 +1,6 @@
+import LabelType from "@/components/label/LabelType";
+import Row from "@/components/table/Row";
 import fetchWithRetry from "@/functions/api";
-import Link from "next/link";
 import { Fragment } from "react";
 
 async function getAnnouncementList() {
@@ -14,26 +15,19 @@ export default async function AnnouncementList() {
   const announcements = await getAnnouncementList();
 
   return (
-    <div className="frame-119">
+    <>
       {announcements.map(({ postingId, title }, index) => (
         <Fragment key={postingId}>
-          <div className="frame-118">
-            <div className="frame-100-2">
-              <div className="frame-93-3">
-                <div className="frame-4-1 background-white border-purple-01">
-                  <p className="caption-12 color-purple-01">마감 미설정</p>
-                </div>
-                <Link className="link" href={`/announcement/${postingId}`}>
-                  <h2 className="h4-20 color-gray-02">{title}</h2>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <Row
+            label={{ type: LabelType.Time, text: "마감 미설정" }}
+            title={{ link: `/announcement/${postingId}`, text: title }}
+            fixed={{ label: { type: LabelType.Default, text: "진행중" } }}
+          />
           {index < announcements.length - 1 ? (
             <div className="line border-gray-05" />
           ) : null}
         </Fragment>
       ))}
-    </div>
+    </>
   );
 }
