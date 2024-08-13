a91d63bc89f2af2b7783e85f28be219753b55177
junho <junho9341@gmail.com>
2024-08-13 22:46:13 +0900

    프로필 페이지 스타일 변경 (#20)
    * 프로필 페이지 자기소개 컴포넌트 스타일 추가
    * MyNearWallet 지갑 연결 버튼 스타일 추가
    * 목록 배치 변경

--- provider/components/blockchain/wallet/NearWallet.jsx
+++ provider/components/blockchain/wallet/NearWallet.jsx
@@ -6,6 +6,8 @@ import "@near-wallet-selector/modal-ui/styles.css";
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import style from "./wallet.module.css";
+import Icon from "@/components/Icon";
 
 export default function NearWallet() {
   const router = useRouter();
@@ -50,12 +52,79 @@ export default function NearWallet() {
   return (
     <>
       {selector == null ? null : !selector.isSignedIn() ? (
-        <button className="blockchain-wallet" onClick={signIn}>
-          Near 지갑 연결하기
-        </button>
+        <div className={style.card_mywallet}>
+          <div className={style.mywallet}>
+            <div className={style.mywallet_text}>
+              <h4 className={style.h4_18}>나의 지갑</h4>
+            </div>
+            <div className={style.wallet_logo_mywallet_information}>
+              <div className={style.wallet_logo}>
+                <Icon
+                  className={style.wallet_logo_white}
+                  src="/Wallet-logo-white.png"
+                  width={61}
+                  height={71}
+                  alt="MyNearWallet Logo"
+                />
+              </div>
+              <div className={style.mywallet_information}>
+                <div className={style.mywallet_information_text}>
+                  <h5 className={style.h5_16}>My Near Wallet</h5>
+                  <p1 className={style.p1_16}>Connect required</p1>
+                </div>
+                <div className={style.button_s}>
+                  <h5>확인하기</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={style.rectangle_19}></div>
+          <div className={style.mywallet_disabled_cover}>
+            <div className={style.mywallet_text}>
+              <h4 className={style.h4_18}>나의 지갑</h4>
+            </div>
+            <div className={style.mywallet_button} onClick={signIn}>
+              <div className={style.button_right_icon}>
+                <h5>지갑 연결하기</h5>
+                <div className={style.icon}>
+                  <Icon
+                    src="/icon_wallet_white.png"
+                    width={24}
+                    height={24}
+                    alt="지갑 연결하기"
+                  ></Icon>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ) : (
-        <div className="blockchain-wallet">
-          <span>{accountId}</span> <button onClick={signOut}>연결해제</button>
+        <div className={style.card_mywallet}>
+          <div className={style.mywallet}>
+            <div className={style.mywallet_text}>
+              <h4 className={style.h4_18}>나의 지갑</h4>
+            </div>
+            <div className={style.wallet_logo_mywallet_information}>
+              <div className={style.wallet_logo}>
+                <Icon
+                  className={style.wallet_logo_white}
+                  src="/Wallet-logo-white.png"
+                  width={61}
+                  height={71}
+                  alt="MyNearWallet Logo"
+                />
+              </div>
+              <div className={style.mywallet_information}>
+                <div className={style.mywallet_information_text}>
+                  <h5 className={style.h5_16}>My Near Wallet</h5>
+                  <p1 className={style.p1_16}>{accountId}</p1>
+                </div>
+                <button className={style.button_s} onClick={signOut}>
+                  <h5>연결해제</h5>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </>
